d9c5be6e8e9ad59212f34e9aca2d27fc49a2861f
solpie <solpie.net@gmail.com>
2015-06-05 14:53:28 +0800

    win32 file watcher

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,22 @@
 __author__ = 'toramisu'
+def singleton(cls):
+    instances = {}
+
+    def get_instance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+
+    return get_instance
+
+
+class Singleton(object):
+    instances = {}
+
+    def __new__(cls, *args, **kwargs):
+        if cls not in cls.instances:
+            cls.instances[cls] = object.__new__(cls)
+        return cls.instances[cls]
+
+
+from .Watcher import Watcher
