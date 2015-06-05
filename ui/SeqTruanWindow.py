0f0ce13fa6588f7bd70a1615d65801bfe8e28132
solpie <solpie.net@gmail.com>
2015-06-05 15:02:37 +0800

    win32 file watcher

---
+++ ui/SeqTruanWindow.py
@@ -0,0 +1,46 @@
+__author__ = 'toramisu'
+from PyQt5.QtGui import QWindow, QPixmap, QIcon, QPainter, QBrush, QColor, QFont
+# from Module.App import App
+from module.Events import *
+from PyQt5.QtWidgets import QMainWindow, QLabel, QWidget
+from ui.Viewport import Viewport
+from PyQt5.QtOpenGL import QGLWidget
+# class SeqTruanWindow(QGLWidget):
+#     def __init__(self, parent=None):
+#         super(SeqTruanWindow, self).__init__( parent)
+class SeqTruanWindow(QMainWindow):
+    def __init__(self):
+        super(SeqTruanWindow, self).__init__()
+        # self.initModule()
+        # app = App()
+        self.resize(1440, 900)
+        # self.paintEvent = self.onPaint
+        self.mainWidget = QWidget()
+        self.__viewport = Viewport(self.mainWidget)
+        self.setCentralWidget(self.mainWidget)
+        self.mouseDoubleClickEvent = self.onDoubleClk
+        self.wheelEvent = self.onWheel
+
+        pass
+
+    # def initModule(self):
+    #     # Event().init()
+    #     # App().init()
+    #     pass
+
+    def onDoubleClk(self, e):
+        self.__viewport.load()
+        pass
+
+    def onWheel(self, e):
+        dy = e.angleDelta().y()
+        if dy > 0:
+            self.__viewport.zoomIn()
+        else:
+            self.__viewport.zoomOut()
+        print('wheel', e.pixelDelta().y(), e.angleDelta())
+        pass
+
+    def onPaint(self, e):
+        print('paint event')
+        pass
---
+++ ui/Transport.py
@@ -0,0 +1,40 @@
+__author__ = 'toramisu'
+
+from PyQt5.QtWidgets import QOpenGLWidget, QWidget, QPushButton, QHBoxLayout, QComboBox
+from module.App import *
+from module.Events import *
+
+
+class Transport(QWidget):
+    def __init__(self, parent):
+        super(Transport, self).__init__(parent)
+        self.hbox = QHBoxLayout(self)
+        self.frameRate = QComboBox()
+        self.frameRate.addItems(['24', '30', '60'])
+        connect(self.frameRate.currentTextChanged, self.onFrameRateChanged)
+        self.hbox.addWidget(self.frameRate)
+
+        self.playButton = QPushButton()
+        self.playButton.setText('play')
+        connect(self.playButton.clicked, self.togglePlay)
+        self.hbox.addWidget(self.playButton)
+
+        self.pauseButton = QPushButton()
+        self.pauseButton.setText('stop')
+        self.hbox.addWidget(self.pauseButton)
+        pass
+
+    def onFrameRateChanged(self, e):
+        fr = int(self.frameRate.currentText())
+        App().sequencePlayback.setFramerate(fr)
+        pass
+
+    def togglePlay(self):
+        state = App().sequencePlayback.state
+        if state == PlayStateType.PLAY:
+            self.playButton.setText('play')
+            Event.dis(PlaybackEvent.STATE, PlayStateType.PAUSE)
+        else:
+            self.playButton.setText('pause')
+            Event.dis(PlaybackEvent.STATE, PlayStateType.PLAY)
+        pass
