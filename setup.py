eaecc5615dcb0342c334ef7cadc7fc63d4791f5c
solpie <solpie.net@gmail.com>
2015-06-14 20:01:14 +0800

    rename setup

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
 # -*- coding: utf-8 -*-
-# A simple setup script to create an executable using PyQt4. This also
-# demonstrates the method for creating a Windows executable that does not have
-# an associated console.
-#
-# PyQt4app.py is a very simple type of PyQt4 application
-#
-# Run the build process by running the command 'python setup.py build'
-#
-# If everything works well you should find a subdirectory in the build
-# subdirectory that contains the files needed to run the application
-
 import sys
 from cx_Freeze import setup, Executable
 
@@ -27,7 +16,7 @@ executables = [
     Executable('Launcher.pyw', base=base)
 ]
 
-setup(name='simple_PyQt4',
+setup(name='SeqTruan',
       version='0.1',
       description='SeqTruan. image sequence view',
       options=options,
