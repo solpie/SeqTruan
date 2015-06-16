c555dadd7ed8e4e7ab13952da591096779e8a2f4
manoyuria <solpie.net@gmail.com>
2015-06-16 22:58:39 +0800

    no  subject.h

--- events/Event.h
+++ events/Event.h
@@ -8,15 +8,44 @@
 #endif //SEQTRUAN_EVENT_H
 
 #include "model/Singleton.h"
-#include "subject.h"
+#include <functional>
+#include <map>
+#include <vector>
+#include <utility> // for std::forward
+template<typename Event>
+class Subject {
+public:
+    Subject() = default;
+
+    template<typename Observer>
+    void registerObserver(const Event &event, Observer &&observer) {
+        observers_[event].push_back(std::forward<Observer>(observer));
+    }
+
+    template<typename Observer>
+    void registerObserver(Event &&event, Observer &&observer) {
+        observers_[std::move(event)].push_back(std::forward<Observer>(observer));
+    }
+
+    void notify(const Event &event) const {
+        for (const auto &obs : observers_.at(event)) obs();
+    }
+
+    // disallow copying and assigning
+    Subject(const Subject &) = delete;
+
+    Subject &operator=(const Subject &) = delete;
+
+private:
+    std::map<Event, std::vector<std::function<void()>>> observers_;
+};
 
 class Evt : public Singleton<Evt> {
-//private:
-//    Subject<std::string> __;
 public:
     Subject<std::string> __;
-    template<typename Observer>
-    static void add(const std::string event, Observer &&observer) {
+
+    template<typename Func>
+    static void add(const std::string event, Func &&observer) {
         Evt::_().__.registerObserver(event, observer);
     }
 
--- model/TrackModel.cpp
+++ model/TrackModel.cpp
@@ -5,32 +5,22 @@
 #include <iostream>
 
 #include "TrackModel.h"
-#include "events/subject.h"
 #include "events/Event.h"
-void foo(TrackModel *i)
-{
+
+void foo(TrackModel *i) {
     i->func1();
 }
+
+void func2(TrackModel *i) {
+    i->func1();
+}
+
 void TrackModel::newTrack() {
-
-    Subject<std::string> s;
-    s.registerObserver("ORANGE", std::bind(foo, this));
-//    __.registerObserver("GREEN", std::bind(this->func1, 0));
-
-//    __.registerObserver("GREEN", &this->func1);
-//    __.notify("GREEN");
-//    __.notify("RED");
-//    __.notify("ORANGE");
-
-//    Evt::_().add("func", std::bind(foo, this));
-//    Evt::_().dis("func");
-
     Evt::add("func", std::bind(foo, this));
+    Evt::add("func", std::bind(func2, this));
     Evt::dis("func");
-
-//    functors.push_back([&] { return 100; });
-//    functors.push_back([&] { return 10; });
 };
-void TrackModel::func1(){
-    std::cout << "foo( "  << " )\n";
+
+void TrackModel::func1() {
+    std::cout << "foo( " << " )\n";
 };
