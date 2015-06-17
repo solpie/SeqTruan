84655fd6d7cde087243a560a05afbd64042368fd
solpie <solpie.net@gmail.com>
2015-06-17 09:34:59 +0800

    open qss

--- model/TrackModel.cpp
+++ model/TrackModel.cpp
@@ -11,24 +11,10 @@ void foo(TrackModel *i)
     i->func1();
 }
 void TrackModel::newTrack() {
-
-//    Subject<std::string> s;
-//    s.registerObserver("ORANGE", std::bind(foo, this));
-//    __.registerObserver("GREEN", std::bind(this->func1, 0));
-
-//    __.registerObserver("GREEN", &this->func1);
-//    __.notify("GREEN");
-//    __.notify("RED");
-//    __.notify("ORANGE");
-
-//    Evt::_().add("func", std::bind(foo, this));
-//    Evt::_().dis("func");
-
     Evt::add("func", std::bind(foo, this));
+//    Evt::add("func", std::bind(foo, this));
     Evt::dis("func");
 
-//    functors.push_back([&] { return 100; });
-//    functors.push_back([&] { return 10; });
 };
 void TrackModel::func1(){
     std::cout << "foo( "  << " )\n";
